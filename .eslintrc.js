caa66d16c97c690efc5a358dd8cf728e30bb094e
ZickZenni <zickzenniyt@gmail.com>
2024-08-29 11:40:56 +0200

    🔧 Disabled no-continue in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': 'error',
     'no-console': 'off',
+    'no-continue': 'off',
   },
   parserOptions: {
     ecmaVersion: 2022,

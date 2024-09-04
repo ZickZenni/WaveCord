fa65e4e4a680692951c804fbf4538a7d4e5e94b2
ZickZenni <zickzenniyt@gmail.com>
2024-09-04 23:59:06 +0200

    🚑️ Fixed missing Message properties

--- src/discord/structures/Message.ts
+++ src/discord/structures/Message.ts
@@ -1,3 +1,9 @@
+import { Snowflake } from './Snowflake';
+import { IUserData } from './user/BaseUser';
+
 export interface Message {
   content: string;
+  timestamp: string;
+  id: Snowflake;
+  author: IUserData;
 }

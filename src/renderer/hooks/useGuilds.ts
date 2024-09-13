ccfef90e8bb208afe126a6ee6f3d291179a9a728
ZickZenni <zickzenniyt@gmail.com>
2024-09-13 08:38:25 +0200

    feat: add useGuilds hook

--- src/renderer/hooks/useGuilds.ts
+++ src/renderer/hooks/useGuilds.ts
@@ -2,17 +2,30 @@ import { useEffect, useState } from 'react';
 import RendererGuild from '../../discord/structures/guild/RendererGuild';
 import { IGuildData } from '../../discord/structures/guild/BaseGuild';
 
+/**
+ * Fetches all guilds the user is in
+ */
 export default function useGuilds() {
   const [guilds, setGuilds] = useState<RendererGuild[]>([]);
 
   useEffect(() => {
-    window.electron.ipcRenderer
-      .invoke('discord:guilds')
-      .then((data: IGuildData[]) => {
-        setGuilds(data.map((v) => new RendererGuild(v)));
-        return true;
-      })
-      .catch((err) => console.error(err));
+    const fetchGuilds = () => {
+      window.electron.ipcRenderer
+        .invoke('discord:guilds')
+        .then((data: IGuildData[]) => {
+          setGuilds(data.map((v) => new RendererGuild(v)));
+          return true;
+        })
+        .catch((err) => console.error(err));
+    };
+
+    const interval = setInterval(fetchGuilds, 2000);
+
+    fetchGuilds();
+
+    return () => {
+      clearInterval(interval);
+    };
   }, []);
 
   return guilds;

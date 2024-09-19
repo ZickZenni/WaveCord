d23b9b73f348da530149d00c7940d8a96eaf00c0
Calvin Rohloff <zickzenni@gmail.com>
2024-09-19 09:23:22 +0200

    feat: sort and view private channels in homepage

--- src/renderer/pages/HomePage/HomePage.tsx
+++ src/renderer/pages/HomePage/HomePage.tsx
@@ -1,12 +1,37 @@
+import DirectMessageButton from '@/components/directmessage/DirectMessageButton';
 import PageLayout from '@/components/page/PageLayout';
 import PageSideBar from '@/components/page/PageSideBar';
+import usePrivateChannels from '@/hooks/usePrivateChannels';
 import { Outlet } from 'react-router-dom';
 
 export default function HomePage() {
+  const channels = usePrivateChannels();
+
   return (
     <PageLayout>
       <div className="HomePage">
-        <PageSideBar />
+        <PageSideBar>
+          {
+            /* Sort private channels by their last message id */
+            channels
+              .sort((a, b) => {
+                if (a.lastMessageId === null) return -1;
+
+                if (b.lastMessageId === null) return 1;
+
+                return Number(a.lastMessageId) - Number(b.lastMessageId);
+              })
+              .reverse()
+              .map((channel) => {
+                return (
+                  <DirectMessageButton
+                    key={`Channel:home:${channel.id}`}
+                    channel={channel}
+                  />
+                );
+              })
+          }
+        </PageSideBar>
         <div className="HomePage--content">
           <Outlet />
         </div>

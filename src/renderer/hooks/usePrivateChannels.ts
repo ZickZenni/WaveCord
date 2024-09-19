2e2e4cb97d77dcb71c779ced1470983d3f7cceab
Calvin Rohloff <zickzenni@gmail.com>
2024-09-19 09:12:31 +0200

    feat: add private channels hook

---
+++ src/renderer/hooks/usePrivateChannels.ts
@@ -0,0 +1,22 @@
+import { IChannelData } from '@/discord/structures/channel/BaseChannel';
+import RendererChannel from '@/discord/structures/channel/RendererChannel';
+import { useEffect, useState } from 'react';
+
+/**
+ * Fetches all private channels from the user
+ */
+export default function usePrivateChannels() {
+  const [channels, setChannels] = useState<RendererChannel[]>([]);
+
+  useEffect(() => {
+    window.electron.ipcRenderer
+      .invoke('discord:private-channels')
+      .then((data: IChannelData[]) => {
+        setChannels(data.map((v) => new RendererChannel(v)));
+        return true;
+      })
+      .catch((err) => console.error(err));
+  });
+
+  return channels;
+}
